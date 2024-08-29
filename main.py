9acd1b034f9b6efafbeb8867a995cd705224852c
lagorg22 <lagorg22@freeuni.edu.ge>
2024-08-29 21:30:20 +0400

    added multiprocessing, made the code drastically faster

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import json
 from pharm import GPC, PSP, Pharmadepot, Aversi
 from flask import Flask, render_template, request
+import concurrent.futures
+import functools
 
-# import pandas as pd
 
 with open('paths.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-
 
 def get_inputs(pharmacy):
     d = data[pharmacy]
     return [d['search_url'], d['photo_xpath'], d['name_xpath'], d['price_xpath'], d['country_xpath'], d['link_xpath']]
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
@@ -24,29 +22,16 @@ def home():
 @app.route('/search', methods=['POST'])
 def search():
     word = request.form.get('search_term')
-
-    gp = GPC(*get_inputs('gpc'), pharmacy='GPC')
-    gpc_infos = gp.show_items(word)
-
-    ps = PSP(*get_inputs('psp'), pharmacy='PSP')
-    ps_infos = ps.show_items(word)
-
-    ph = Pharmadepot(*get_inputs('pharmadepot'), pharmacy='Pharmadepot')
-    ph_infos = ph.show_items(word)
-
-    av = Aversi(*get_inputs('aversi'), pharmacy='Aversi')
-    av_infos = av.show_items(word)
-
-    items_infos = gpc_infos + av_infos + ph_infos + ps_infos
-
-    items_infos = sorted(items_infos, key=lambda x: x['Price'])
+    items_infos = []
+    partial_get_info = functools.partial(get_info, word=word, items_info=items_infos)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(partial_get_info, ['GPC', 'PSP', 'Pharmadepot', 'Aversi'])
 
     return render_template('index.html', infos=items_infos)
 
-#
+def get_info(obj, word, items_info):
+    o = globals()[obj](*get_inputs(obj.lower()), pharmacy=obj)
+    items_info.extend(o.show_items(word))
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-# av = Aversi(*get_inputs('aversi'))
-# av_infos = av.show_items('ana')
-# print(av_infos)
